bd00d2bd8c66f1c23e3c8bd802c5a7217205b6ed
Hristo Roque <aroquega@unsa.edu.pe>
2020-08-07 20:01:04 -0500

    Agregando el GameManager

---
+++ OratoriaVR/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    OnMenu,
+    OnPause,
+    OnPlay,
+};
+
+public class GameManager : MonoBehaviour
+{
+    public GameState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        state = GameState.OnPause;
+    }
+}
